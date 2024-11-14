e3d1480172ba3f2ee2db3574a81436193cac9e8b
Madhvendra <madhvendrasingh32@gmail.com>
2024-11-14 22:21:45 +0530

    Initial Commit

---
+++ beta/betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  betaApp.swift
+//  beta
+//
+//  Created by Madhvendra Singh on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ betaTests/betaTests.swift
@@ -0,0 +1,17 @@
+//
+//  betaTests.swift
+//  betaTests
+//
+//  Created by Madhvendra Singh on 14/11/24.
+//
+
+import Testing
+@testable import beta
+
+struct betaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
